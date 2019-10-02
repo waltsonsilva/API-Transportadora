e15d875c75cc332c48ae49bcff58ec60d674422f
waltson silva <waltson.silva@hotmail.com>
2019-10-01 23:17:40 -0300

    Validações dos campos da entidade trasnportadoras.

--- src/main/java/br/com/transp/beans/Transportadora.java
+++ src/main/java/br/com/transp/beans/Transportadora.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import br.com.transp.enums.EstadosBrasil;
 import br.com.transp.enums.Modal;
@@ -24,18 +28,28 @@ public class Transportadora implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigoId;
+	@NotEmpty
+    @Pattern(regexp = ".+@.+\\.[a-z]+")
 	private String email;
+	@NotEmpty
 	private String nome;
+	@NotEmpty
 	private String empresa;
 	private String telefone;
 	private String celular;
 	private String whatsapp;
+	@NotNull
 	private Modal modal;
 	private String cep;
+	@NotNull
 	private EstadosBrasil estado;
+	@NotEmpty
 	private String cidade;
+	@NotEmpty
 	private String bairro;
+	@NotEmpty
 	private String logradouro;
+	@NotEmpty
 	private String numeroLogradouro;
 	private int termoServico;
 
